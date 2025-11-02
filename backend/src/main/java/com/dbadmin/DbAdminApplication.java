3f796cb8d3936e06c3ae29be14d5105f30e7c93e
qusc <2840927713@qq.com>
2025-11-03 01:50:45 +0800

    feat(backend): 添加应用启动日志和访问地址提示
    - 在 DbAdminApplication 中引入 AiService 和日志工具
    - 添加应用启动后打印访问地址的日志信息
    - 优化代码结构，提升可读性

--- backend/src/main/java/com/dbadmin/DbAdminApplication.java
+++ backend/src/main/java/com/dbadmin/DbAdminApplication.java
@@ -1,11 +1,20 @@
 package com.dbadmin;
 
+import com.dbadmin.service.AiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DbAdminApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(DbAdminApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(DbAdminApplication.class, args);
+
+        // 打印访问地址：
+        log.info("访问地址：http://localhost:8080/index.html");
     }
 }
